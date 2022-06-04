571023bdebab5ea168f0972f8efad4d19e105795
nisa_ashfiyani <ashfii.yaa22@gmail.com>
2022-06-04 09:36:45 +0700

    suratmasuk

--- arsip/include/config.php
+++ arsip/include/config.php
@@ -1,14 +1,5 @@
 <?php
-$conn_str = "host=tiny.db.elephantsql.com " .
-    "port=5432 " .
-    "user=wxzegmyx " .
-    "dbname=wxzegmyx " .
-    "password=EYwcxPspNgYgxsvUklZKn_TaHaZXj4NK";
-$conn = pg_connect($conn_str);
-
-if($conn) {
-    echo "<h3>Koneksi Berhasil</h3>";
-} else {
-    echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
-}
-?>
+$host     = "localhost";    // Nama host
+$username = "root";         // Username database
+$password = "";   // Password database
+$database = "arsip";   // Nama database
